288a2849b8a0edbd837ab3393a39ae5e43f39f3d
audry <audry.dupont@condorcet.be>
2025-09-19 21:21:48 +0200

    try FCM 4

--- index.js
+++ index.js
@@ -6,16 +6,26 @@ const admin = require("firebase-admin");
 const app = express();
 app.use(express.json());
 
-// ===== INIT FIREBASE ADMIN =====
-const serviceAccount = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT);
-
-// Remplace les \n littéraux par de vrais retours à la ligne
-serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+// ===== INIT FIREBASE ADMIN (sans variable d'environnement) =====
+const serviceAccount = {
+    "type": "service_account",
+    "project_id": "ctt-frameries",
+    "private_key_id": "c57ba3b66e3099ed0214d4c59e7dbf144bedf16f",
+    "private_key": `-----BEGIN PRIVATE KEY-----
+MIIEvgIBADAN...
+-----END PRIVATE KEY-----`,
+    "client_email": "firebase-adminsdk-fbsvc@ctt-frameries.iam.gserviceaccount.com",
+    "client_id": "112517379689733232787",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc@ctt-frameries.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
+};
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 });
-
 
 // ===== JSON SERVER =====
 const router = jsonServer.router(path.join("/data", "db.json"));
@@ -30,14 +40,7 @@ app.post("/sendNotification", async (req, res) => {
     const { token, title, body } = req.body;
 
     try {
-        const message = {
-            token,
-            notification: {
-                title,
-                body,
-            },
-        };
-
+        const message = { token, notification: { title, body } };
         const response = await admin.messaging().send(message);
         res.json({ success: true, response });
     } catch (err) {
